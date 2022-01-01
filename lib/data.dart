61cc5192a0f2d4631a109cdacb8c9ee1255ef74e
oobaidhashmi089 <oobaidhashmi089@gmail.com>
2022-01-01 22:54:14 +0500

    aaaaa

--- lib/data.dart
+++ lib/data.dart
@@ -7,5 +7,6 @@
 
 // Napoleon
 String myId = 'YB0XmxZ7KiZTHGtGRpue';
-String myUsername = 'Barack Obama';
-String myUrlAvatar = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/480px-President_Barack_Obama.jpg';
+String myUsername = 'obaid hashmi';
+String myUrlAvatar =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/480px-President_Barack_Obama.jpg';
