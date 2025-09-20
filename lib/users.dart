953512fcbd603abe1e7b321decdf442fa8f28a48
Syed Obaid Hashmi <79754399+oobaidhashmi089@users.noreply.github.com>
2025-09-20 20:57:17 +0500

    Update users.dart

--- lib/users.dart
+++ lib/users.dart
@@ -1,72 +1,74 @@
 import 'package:firebase_chat_example/model/user.dart';
 
 class Users {
-  static get initUsers => <User>[
-        User(
-          name: 'obaid hashmi',
-          urlAvatar:
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/480px-President_Barack_Obama.jpg',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'hamza yaseen',
-          urlAvatar:
-              'https://upload.wikimedia.org/wikipedia/commons/9/9b/Andrea_Appiani_Napoleon_K%C3%B6nig_von_Rom.jpg',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Ali ahmed',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Rahhat',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1496203695688-3b8985780d6a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=641&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Caren ',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Muqeet',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Tom ',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1586083702768-190ae093d34d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=695&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Sarah',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1492106087820-71f1a00d2b11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Johanna Jackson',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=991&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'James Schmidt',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=879&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-        User(
-          name: 'Anton Wagner',
-          urlAvatar:
-              'https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          lastMessageTime: DateTime.now(),
-        ),
-      ];
+  static final DateTime _now = DateTime.now();
+
+  static final List<User> initUsers = [
+    const User(
+      name: 'Obaid Hashmi',
+      urlAvatar:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/480px-President_Barack_Obama.jpg',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Hamza Yaseen',
+      urlAvatar:
+          'https://upload.wikimedia.org/wikipedia/commons/9/9b/Andrea_Appiani_Napoleon_K%C3%B6nig_von_Rom.jpg',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Ali Ahmed',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Rahhat',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1496203695688-3b8985780d6a?ixlib=rb-1.2.1&auto=format&fit=crop&w=641&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Caren',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Muqeet',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Tom',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1586083702768-190ae093d34d?ixlib=rb-1.2.1&auto=format&fit=crop&w=695&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Sarah',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1492106087820-71f1a00d2b11?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Johanna Jackson',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixlib=rb-1.2.1&auto=format&fit=crop&w=991&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'James Schmidt',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixlib=rb-1.2.1&auto=format&fit=crop&w=879&q=80',
+      lastMessageTime: _now,
+    ),
+    const User(
+      name: 'Anton Wagner',
+      urlAvatar:
+          'https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+      lastMessageTime: _now,
+    ),
+  ];
 }
